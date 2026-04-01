b52b3ba76aa4e100c8f5351f6e62c314b21817d2
polygonstew <gamedevdata@gmail.com>
2026-04-01 11:45:03 -0400

    rules :: round 3

--- CSharpProjects/StreetDice/Program.cs
+++ CSharpProjects/StreetDice/Program.cs
@@ -23,7 +23,7 @@ var title = """
          /        \|  |  |  | \/\  ___/\  ___/|  |    |    `   \  \  \__\  ___/ 
         /_______  /|__|  |__|    \___  >\___  >__|   /_______  /__|\___  >___  >
                 \/                   \/     \/               \/        \/    \/ 
-
+                                2 Players Only
 """;
 
 // Slogan Selection
@@ -82,34 +82,35 @@ static void RollDie(int roll)
         
     }
 } 
-// I don't get functions yet obvliously !!!!!!!!!!! OMG really. 
-// these babies wanna stack.
 
-/*/ rewards
-!!! I don't know about these. I want more of a competitive game
-int totalScore = roll1 + roll2;
 
-if (roll1 == 1 && roll2 == 1)
-{
-    Console.WriteLine("Snake Eyes! You lose everything!");
-}
-else if (roll1 == roll2)
-{
-    Console.WriteLine("Doubles! You get to roll again!");
-}
-else if (totalScore == 7 || totalScore == 11)
-{
-    Console.WriteLine("Winner winner, chicken dinner!");
-}
-else
-{
-    Console.WriteLine($"You scored {totalScore}. Nothing special happens.");
-}
-*/
+/*/ Game Rules:
+ 2 dice and cash   
+ pick a shooter - ask player for H or L high or low, winner is the closest to either H or L
+    !- Program in a H or L choice
+betting system - start with 100 dollars, bet on each round, win or lose money based on outcome
+    !- Program in a betting system and asign each player 100 dollars to start
+gotta match or fade (put down or pass)
+first roll is THE COMEOUT if Shooter rolls(7 or 11 ) !!- WINS THE POT
+                      if Shooter rolls (2, 3, or 12) !!- LOSES THE POT
+ first round is the COMEOUT, if the shooter rolls (4, 5, 6, 8, 9, or 10) that number becomes the POINT
+    - Shooter keeps rolling until they roll the POINT again !!!-(WIN) or a 7 !!!-(LOSE) 
+    pass to the next player and repeat until one player has all the money or everyone but one player is broke.
+    FADE - if you think the shooter will lose, you can choose to ???-FADE and ???-BET against them. 
+    If they win, you lose your bet, if they lose, you win your bet. 
+    You can also choose to match and bet with the shooter. 
+    If they win, you win your bet, if they lose, you lose your bet.
+ 
+
+
+ /*/
+
+
 //player setup
 string askPlayer = "Are you ready to play? (y/n)";
-string howManyPlayers = "How many players? (1-4)";
-int playerCount = 0;
+// string howManyPlayers = "How many players? (1-4)";
+string hL = "Someone pick High or Low! (H/L)";
+// int playerCount = 0;
 
 //asking
 
@@ -118,7 +119,8 @@ string play = Console.ReadLine().ToLower();
 if (play == "y") {
         
         
-    Console.WriteLine("Welcome to the streets! Let's get you set up!");
+    Console.WriteLine("Welcome to the streets! Let's get you set up!\n");
+    Console.Write(hL);
     }
     else if (play == "n") {
         Console.WriteLine("Maybe next time!.... punk");
@@ -128,15 +130,40 @@ if (play == "y") {
     }   
      
 // getting sloppy 
-
-Console.WriteLine(howManyPlayers);
-playerCount = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Great! We have {playerCount} players ready to go!");
-
-
+bool shooterChosen = false;
+// Console.WriteLine(howManyPlayers);
+// if YES
+string hLC = (Console.ReadLine());
+Console.WriteLine($"Great! {hLC} it is! Now let's see what you will roll...");
 RollDie(roll1);
 RollDie(roll2);
 Console.WriteLine($"You rolled: {roll1} and {roll2}");
+int shooter1 = roll1 + roll2;
+Console.WriteLine("Player 2 rolls...");
+RollDie(roll1);
+RollDie(roll2);
+Console.WriteLine($"You rolled: {roll1} and {roll2}");
+int shooter2 = roll1 + roll2;
+if (shooter1 > shooter2 && hLC == "H") {
+        Console.WriteLine("Player 1 wins! You are the SHOOTER!");
+        shooterChosen = true;
+    }
+    else if (shooter1 < shooter2 && hLC == "L") {
+        Console.WriteLine("Player 1 wins! You are the SHOOTER!");
+        shooterChosen = true;
+    }
+    else if (shooter1 > shooter2 && hLC == "L") {
+        Console.WriteLine("Player 2 wins! You are the SHOOTER!");
+        shooterChosen = false;
+    }
+    else if (shooter1 < shooter2 && hLC == "H") {
+        Console.WriteLine("Player 2 wins! You are the SHOOTER!");
+        shooterChosen = false;
+    }
+    else {
+        Console.WriteLine("It's a tie! Roll again!");
+    }
+
 // 
 // i'm getting double rolls for some reason
 // going to use VSCode's Source Control for the first time, in this project.
