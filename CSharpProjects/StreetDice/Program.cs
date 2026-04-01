d6d652e810191d95cc55f74760b75a2abae50344
polygonstew <gamedevdata@gmail.com>
2026-04-01 08:19:42 -0400

    1st up :: round 1

---
+++ CSharpProjects/StreetDice/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+Console.WriteLine("                     This isn't momma's street corner anymore!");
+Console.WriteLine("  _________ __                         __    ________  .__             "); 
+Console.WriteLine("/   _____//  |________   ____   _____/  |_  \\______ \\ |__| ____  ____  ");
+Console.WriteLine("\\_____  \\   __\\\\_  __ \\_/ __ \\_/ __ \\   __\\  |    |  \\|  |/ ___\\/ __ \\ ");
+Console.WriteLine(" /       \\|  |  |  | \\/\\  ___/\\  ___/|  |    |    `   \\  \\  \\__\\  ___/ ");
+Console.WriteLine("/______  /|__|  |__|     \\___  >\\___  >__|   /_______  /__|\\___  >___  >");
+Console.WriteLine("       \\/                    \\/     \\/               \\/        \\/    \\/ ");
+Console.WriteLine("                                                                  Street Dice ");
+Console.WriteLine("                                                              v0.00000000001a");
+               
+               
+
+var title = """
+                    This isn't momma's street corner anymore!
+         _________  __                         __    ________  .__            
+        /   _____/ /  |________   ____   _____/  |_  \______ \ |__| ____  ____  
+        \_____  \\    __\_  __ \_/ __ \_/ __ \   __\  |    |  \|  |/ ___\/ __ \ 
+         /        \|  |  |  | \/\  ___/\  ___/|  |    |    `   \  \  \__\  ___/ 
+        /_______  /|__|  |__|    \___  >\___  >__|   /_______  /__|\___  >___  >
+                \/                   \/     \/               \/        \/    \/ 
+                                                                       Street Dice 
+                                                                    v0.00000000001a
+""";
+
+Console.WriteLine(title);
